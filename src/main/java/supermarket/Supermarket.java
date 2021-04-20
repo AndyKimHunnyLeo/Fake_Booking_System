d63a4a70c87e7a25696bcc8597925f7c8ba2c018
Andy Kim <hyungSeok.kim@outlook.com>
2021-04-20 20:16:59 +1200

    When booking added now has limit of 8 for each hour

--- src/main/java/supermarket/Supermarket.java
+++ src/main/java/supermarket/Supermarket.java
@@ -6,6 +6,7 @@ import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Supermarket {
     private List<Booking> bookIngList = new ArrayList<>();
@@ -27,6 +28,10 @@ public class Supermarket {
     }
 
     public void addBookIngList(Booking booking) {
+        List<Booking> dateCheck = this.bookIngList.stream().filter(booking1 -> booking1.getDay().equals(booking.getDay())).collect(Collectors.toList());
+        if(dateCheck.size() >= 8){
+             throw new IllegalArgumentException("Already fully booked");
+        }
         this.bookIngList.add(booking);
     }
 
